a32787a3e61d189408064d612dd8140ef5035687
agloks <perdi9090@yahoo.com>
2020-07-26 19:34:33 -0300

    add: done 50% match

--- src/pages/match_page.js
+++ src/pages/match_page.js
@@ -1,24 +1,79 @@
 import React from "react";
 import Loading from "../components/loading"
+import RecipeReviewCard from "../components/CardsMatch/cardmatch"
 
 import { setUserStorage, getUserStorage, clearUserStorage } from "../utils/loginStorage"
 import { accountGetGrant } from "../utils/handlerAccountAPI"
 
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+
+const useStyles = (theme) => ({
+  root: {
+    flexGrow: 1,
+    height: "100vh"
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+  recipeView: {
+    margin: "auto",
+    backgroundColor: "red"
+  }
+});
+
 class MatchPage extends React.Component {
   constructor(props) {
     super(props);
+    this.classes = props.classes
     this.state = {
       loading: true
     };
   }
 
   render() {
+    console.log(this.classes.root)
+
     return (
       <>
-        <Loading loadingState={this.state.loading} />
+      <Grid container 
+      className={this.classes.root} 
+      spacing={8} 
+      justify={"center"}
+      alignContent={"center"}
+      alignItems={"center"}
+      >
+
+        <Grid item xs={12} md={5}>
+          <Paper className={this.classes.control}>
+            <Grid container>
+              <Grid item className={this.classes.recipeView} >
+                <RecipeReviewCard  />
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} md={5}>
+          <Paper className={this.classes.control}>
+            <Grid container>
+              <Grid item className={this.classes.recipeView} >
+                <RecipeReviewCard />
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+
+      </Grid>
       </>
     );
   }
 }
 
-export default MatchPage
+export default withStyles(useStyles)(MatchPage)
